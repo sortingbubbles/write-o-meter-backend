d62a0e4f06166f8d4e3c7f4a2a7d8960f09bf48e
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-02-16 01:15:47 +0100

    feat: update origins

--- write-o-meter/main.py
+++ write-o-meter/main.py
@@ -8,10 +8,8 @@ class Text(BaseModel):
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
+    "https://sortingbubbles.github.io",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
