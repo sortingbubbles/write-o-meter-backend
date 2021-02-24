a6ff38a52a43beac9abce0d01a3543c7c94a0146
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-02-24 01:23:03 +0100

    feat: import spacy!

--- write-o-meter/main.py
+++ write-o-meter/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import spacy
+
+nlp = spacy.load("el_core_news_sm")
 
 class AnalysisRequestBody(BaseModel):
     text: str
@@ -22,4 +27,12 @@ app.add_middleware(
 
 @app.post("/analyze")
 async def analyze_text(request: AnalysisRequestBody):
-    return 'Λέξεις: ' + str(len(request.text.split())) + ' & Προτάσεις: ' + str(len(request.text.split('.')) - 1)
+    doc = nlp(request.text)
+    print(doc, 'hello')
+    print(doc.text)
+    for token in doc:
+        print(token.text, token.pos_, token.dep_)
+    words = [token.text for token in doc if token.is_punct != True]
+    res = doc.to_json()
+    res['words'] = words
+    return res
